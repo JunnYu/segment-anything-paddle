d123e0977d289ff7ddf3c41e1c5bbfc0e6978fdc
yujun <573009727@qq.com>
2023-04-08 22:31:36 +0800

    add TODO fastdeploy

---
+++ scripts/export_fastdeploy_model.py
@@ -0,0 +1,147 @@
+# TODO
+# # Copyright (c) Meta Platforms, Inc. and affiliates.
+# # All rights reserved.
+
+# # This source code is licensed under the license found in the
+# # LICENSE file in the root directory of this source tree.
+
+# import paddle
+# import paddle.nn as nn
+
+# from segment_anything_paddle import sam_model_registry
+# from segment_anything_paddle.utils.fd_model import SamFastdeployModel
+
+# import argparse
+
+# try:
+#     import fastdeploy  # type: ignore
+
+#     fastdeploy_exists = True
+# except ImportError:
+#     fastdeploy_exists = False
+
+# parser = argparse.ArgumentParser(
+#     description="Export the SAM prompt encoder and mask decoder to an FastDeploy model."
+# )
+
+# parser.add_argument(
+#     "--checkpoint", type=str, required=True, help="The path to the SAM model checkpoint."
+# )
+
+# parser.add_argument(
+#     "--output", type=str, required=True, help="The filename to save the FastDeploy model to."
+# )
+
+# parser.add_argument(
+#     "--model-type",
+#     type=str,
+#     required=True,
+#     help="In ['default', 'vit_h', 'vit_l', 'vit_b']. Which type of SAM model to export.",
+# )
+
+# parser.add_argument(
+#     "--return-single-mask",
+#     action="store_true",
+#     help=(
+#         "If true, the exported ONNX model will only return the best mask, "
+#         "instead of returning multiple masks. For high resolution images "
+#         "this can improve runtime when upscaling masks is expensive."
+#     ),
+# )
+
+# parser.add_argument(
+#     "--gelu-approximate",
+#     action="store_true",
+#     help=(
+#         "Replace GELU operations with approximations using tanh. Useful "
+#         "for some runtimes that have slow or unimplemented erf ops, used in GELU."
+#     ),
+# )
+
+# parser.add_argument(
+#     "--use-stability-score",
+#     action="store_true",
+#     help=(
+#         "Replaces the model's predicted mask quality score with the stability "
+#         "score calculated on the low resolution masks using an offset of 1.0. "
+#     ),
+# )
+
+# parser.add_argument(
+#     "--return-extra-metrics",
+#     action="store_true",
+#     help=(
+#         "The model will return five results: (masks, scores, stability_scores, "
+#         "areas, low_res_logits) instead of the usual three. This can be "
+#         "significantly slower for high resolution outputs."
+#     ),
+# )
+
+
+# def run_export(
+#     model_type: str,
+#     checkpoint: str,
+#     output: str,
+#     return_single_mask: bool,
+#     gelu_approximate: bool = False,
+#     use_stability_score: bool = False,
+#     return_extra_metrics=False,
+# ):
+#     print("Loading model...")
+#     sam = sam_model_registry[model_type](checkpoint=checkpoint)
+
+#     fastdeploy_model = SamFastdeployModel(
+#         model=sam,
+#         return_single_mask=return_single_mask,
+#         use_stability_score=use_stability_score,
+#         return_extra_metrics=return_extra_metrics,
+#     )
+
+#     if gelu_approximate:
+#         for n, m in fastdeploy_model.named_sublayers(include_self=True):
+#             if isinstance(m, nn.GELU):
+#                 m._approximate = True
+
+#     dynamic_axes = {
+#         "point_coords": {1: "num_points"},
+#         "point_labels": {1: "num_points"},
+#     }
+
+#     embed_dim = sam.prompt_encoder.embed_dim
+#     embed_size = sam.prompt_encoder.image_embedding_size
+#     mask_input_size = [4 * x for x in embed_size]
+#     dummy_inputs = {
+#         "image_embeddings": paddle.randn([1, embed_dim, *embed_size], dtype=paddle.float32),
+#         "point_coords": paddle.randint(low=0, high=1024, shape=(1, 5, 2), dtype=paddle.float32),
+#         "point_labels": paddle.randint(low=0, high=4, shape=(1, 5), dtype=paddle.float32),
+#         "mask_input": paddle.randn([1, 1, *mask_input_size], dtype=paddle.float32),
+#         "has_mask_input": paddle.to_tensor([1], dtype=paddle.float32),
+#         "orig_im_size": paddle.to_tensor([1500, 2250], dtype=paddle.float32),
+#     }
+
+#     _ = fastdeploy_model(**dummy_inputs)
+
+#     output_names = ["masks", "iou_predictions", "low_res_masks"]
+
+#     if fastdeploy_exists:
+#         fd_inputs = {k: to_numpy(v) for k, v in dummy_inputs.items()}
+#         fd_session = InferenceSession(output)
+#         _ = fd_session.run(None, fd_inputs)
+#         print("Model has successfully been run with FastDeploy.")
+
+
+# def to_numpy(tensor):
+#     return tensor.cpu().numpy()
+
+
+# if __name__ == "__main__":
+#     args = parser.parse_args()
+#     run_export(
+#         model_type=args.model_type,
+#         checkpoint=args.checkpoint,
+#         output=args.output,
+#         return_single_mask=args.return_single_mask,
+#         gelu_approximate=args.gelu_approximate,
+#         use_stability_score=args.use_stability_score,
+#         return_extra_metrics=args.return_extra_metrics,
+#     )
---
+++ segment_anything_paddle/utils/fd_model.py
@@ -0,0 +1,154 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import paddle
+
+import paddle.nn as nn
+from paddle.nn import functional as F
+
+from typing import Tuple
+
+from ..modeling import Sam
+from .amg import calculate_stability_score
+
+
+class SamFastdeployModel(nn.Layer):
+    """
+    This model should not be called directly, but is used in FastDeploy export.
+    It combines the prompt encoder, mask decoder, and mask postprocessing of Sam,
+    with some functions modified to enable model tracing. Also supports extra
+    options controlling what information. See the FastDeploy export script for details.
+    """
+
+    def __init__(
+        self,
+        model: Sam,
+        return_single_mask: bool,
+        use_stability_score: bool = False,
+        return_extra_metrics: bool = False,
+    ) -> None:
+        super().__init__()
+        self.mask_decoder = model.mask_decoder
+        self.model = model
+        self.img_size = model.image_encoder.img_size
+        self.return_single_mask = return_single_mask
+        self.use_stability_score = use_stability_score
+        self.stability_score_offset = 1.0
+        self.return_extra_metrics = return_extra_metrics
+
+    @staticmethod
+    def resize_longest_image_size(
+        input_image_size: paddle.Tensor, longest_side: int
+    ) -> paddle.Tensor:
+        input_image_size = input_image_size.cast(paddle.float32)
+        scale = longest_side / paddle.max(input_image_size)
+        transformed_size = scale * input_image_size
+        transformed_size = paddle.floor(transformed_size + 0.5).cast(paddle.int64)
+        return transformed_size
+
+    def _embed_points(self, point_coords: paddle.Tensor, point_labels: paddle.Tensor) -> paddle.Tensor:
+        point_coords = point_coords + 0.5
+        point_coords = point_coords / self.img_size
+        point_embedding = self.model.prompt_encoder.pe_layer._pe_encoding(point_coords)
+        point_labels = point_labels.unsqueeze(-1).expand_as(point_embedding)
+
+        point_embedding = point_embedding * (point_labels != -1).cast(point_embedding.dtype)
+        point_embedding = point_embedding + self.model.prompt_encoder.not_a_point_embed.weight * (
+            point_labels == -1
+        )
+
+        for i in range(self.model.prompt_encoder.num_point_embeddings):
+            point_embedding = point_embedding + self.model.prompt_encoder.point_embeddings[
+                i
+            ].weight * (point_labels == i)
+
+        return point_embedding
+
+    def _embed_masks(self, input_mask: paddle.Tensor, has_mask_input: paddle.Tensor) -> paddle.Tensor:
+        mask_embedding = has_mask_input * self.model.prompt_encoder.mask_downscaling(input_mask)
+        mask_embedding = mask_embedding + (
+            1 - has_mask_input
+        ) * self.model.prompt_encoder.no_mask_embed.weight.reshape([1, -1, 1, 1])
+        return mask_embedding
+
+    def mask_postprocessing(self, masks: paddle.Tensor, orig_im_size: paddle.Tensor) -> paddle.Tensor:
+        masks = F.interpolate(
+            masks,
+            size=(self.img_size, self.img_size),
+            mode="bilinear",
+            align_corners=False,
+        )
+
+        prepadded_size = self.resize_longest_image_size(orig_im_size, self.img_size)
+        masks = masks[..., : int(prepadded_size[0]), : int(prepadded_size[1])]
+
+        orig_im_size = orig_im_size.cast(paddle.int64)
+        h, w = orig_im_size[0], orig_im_size[1]
+        masks = F.interpolate(masks, size=(h, w), mode="bilinear", align_corners=False)
+        return masks
+
+    def select_masks(
+        self, masks: paddle.Tensor, iou_preds: paddle.Tensor, num_points: int
+    ) -> Tuple[paddle.Tensor, paddle.Tensor]:
+        # Determine if we should return the multiclick mask or not from the number of points.
+        # The reweighting is used to avoid control flow.
+        score_reweight = paddle.to_tensor(
+            [[1000] + [0] * (self.model.mask_decoder.num_mask_tokens - 1)]
+        )
+        score = iou_preds + (num_points - 2.5) * score_reweight
+
+        # TODO junnyu
+        best_idx = paddle.argmax(score, axis=1, dtype="int32")
+        bs_range = paddle.arange(masks.shape[0], dtype="int32")
+        masks = masks.gather_nd(
+            paddle.stack([bs_range, best_idx], axis=-1)
+        ).unsqueeze(1)
+        iou_preds = iou_preds.gather_nd(
+            paddle.stack([bs_range, best_idx], axis=-1)
+        ).unsqueeze(1)
+        # masks = masks[paddle.arange(masks.shape[0]), best_idx, :, :].unsqueeze(1)
+        # iou_preds = iou_preds[paddle.arange(masks.shape[0]), best_idx].unsqueeze(1)
+
+        return masks, iou_preds
+
+    @paddle.no_grad()
+    def forward(
+        self,
+        image_embeddings: paddle.Tensor,
+        point_coords: paddle.Tensor,
+        point_labels: paddle.Tensor,
+        mask_input: paddle.Tensor,
+        has_mask_input: paddle.Tensor,
+        orig_im_size: paddle.Tensor,
+    ):
+        sparse_embedding = self._embed_points(point_coords, point_labels)
+        dense_embedding = self._embed_masks(mask_input, has_mask_input)
+
+        masks, scores = self.model.mask_decoder.predict_masks(
+            image_embeddings=image_embeddings,
+            image_pe=self.model.prompt_encoder.get_dense_pe(),
+            sparse_prompt_embeddings=sparse_embedding,
+            dense_prompt_embeddings=dense_embedding,
+        )
+
+        if self.use_stability_score:
+            scores = calculate_stability_score(
+                masks, self.model.mask_threshold, self.stability_score_offset
+            )
+
+        if self.return_single_mask:
+            masks, scores = self.select_masks(masks, scores, point_coords.shape[1])
+
+        upscaled_masks = self.mask_postprocessing(masks, orig_im_size)
+
+        if self.return_extra_metrics:
+            stability_scores = calculate_stability_score(
+                upscaled_masks, self.model.mask_threshold, self.stability_score_offset
+            )
+            areas = (upscaled_masks > self.model.mask_threshold).sum(-1).sum(-1)
+            return upscaled_masks, scores, stability_scores, areas, masks
+
+        return upscaled_masks, scores, 
